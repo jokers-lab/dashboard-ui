6930a3f9f15de0da311e1332180a089189120476
harshawije-dev <harsha.bro1997@gmail.com>
2023-06-29 18:10:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="w-full pr-4">
+      <Sidebar />
+      <div className="w-[75%] h-full"></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+const Sidebar = () => {
+  return (
+    <div className="w-1/4 h-screen bg-slate-800 flex flex-col gap-2 py-5">
+        <article className="px-2 py-4 flex justify-center items-center">
+            <h2 className="font-bold text-xl text-white">Gameset</h2>
+        </article>
+    </div>
+  )
+};
+
+export default Sidebar;
